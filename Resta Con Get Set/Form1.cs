7648995c7a8f570583d7ae85f876b88c2a91e004
JesusAdanOp <jesusadanorozcop@outlook.com>
2022-09-21 07:29:41 -0600

    Agregar archivos de proyecto.

---
+++ Resta Con Get Set/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Resta_Con_Get_Set
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clresta resta = new clresta();
+            resta.V1 = int.Parse(txtv1.Text);
+            resta.V2 = int.Parse(txtv2.Text);
+
+            txtresult.Text = resta.calcular().ToString();
+        }
+    }
+}
---
+++ Resta Con Get Set/clresta.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resta_Con_Get_Set
+{
+    internal class clresta
+    {
+        int v1, v2;
+
+        public int V1
+        {
+            get { return v1; }
+            set { v1 = value; }
+        }
+
+        public int V2
+        {
+            get { return v2; }
+            set { v2 = value; }
+        }
+
+        public int calcular()
+        {
+            return v1 - v2;
+        }
+
+    }
+}
